0cb5d9a0de3875cc93994961ebdf43c4847b5529
Im-Edijs <edijsbikernieks@gmail.com>
2023-11-24 14:51:34 +0200

    4 uzdevums

--- 1.uzdevums.py
+++
@@ -1,7 +0,0 @@
-ievads = int(input("Ievadiet skaitli:"))
-
-sakums=1
-
-for i in range(1, ievads+1):
-    sakums += 1
-    print(i)
--- 2.uzdevums.py
+++
@@ -1,4 +0,0 @@
-sk =  1
-while sk**2 <= 1000:
-    sk +=   1
-print(f"Skaitlis kura kvadrāts ir lielaks par 1000 ir: {sk}") 
